d9113db5c8b332abaa801a6ec2bd2e529796a9b0
Andrew Herculano <andrewherculano@hotmail.com>
2020-09-19 23:35:20 -0300

    Adding graphics

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './assets/pages/Home';
 import Records from './assets/pages/Records';
+import Charts from './assets/pages/Charts';
 import Header from './components/Header';
 
 const Routes = () => (
@@ -13,6 +14,9 @@ const Routes = () => (
             </Route>
             <Route path="/records">
                 <Records />
+            </Route>
+            <Route path="/charts">
+                <Charts />
             </Route>
         </Switch>
     </BrowserRouter>
